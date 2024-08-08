31d38b083a03e37e631630f7c2476186ac958622
Beverly <beverlymateo11@gmail.com>
2024-08-08 14:06:46 +0800

    installed axios

--- index.js
+++ index.js
@@ -4,4 +4,16 @@
 //   console.log("Hello, World!");
 // });
 
+const axios = require("axios");
+
+// Make a GET request to a public API
+axios
+  .get("https://jsonplaceholder.typicode.com/todos/1")
+  .then((response) => {
+    console.log("DATA FROM API:", response.data);
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
+  });
+
 console.log("Hello, World!" + new Date());
